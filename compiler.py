88ad9d4cbb79d2d18e042c74e8a311e252dd4fa5
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-04-28 14:51:28 +0900

    renewal

--- compiler.py
+++ compiler.py
@@ -10,7 +10,8 @@ def sign_ext(d, m=8):
 
 def parse_addr(addr):
     m = re.match(r'(\-?[0-9]+)\(([0-7])\)', addr)
-    return int(m.group(2)), int(m.group(1))
+    return int(m.group(2)), sign_ext(int(m.group(1)))
+
 
 def parse_line(line):
     ARIN = {
@@ -46,74 +47,29 @@ def parse_line(line):
         'bne': 0b011,
     }
 
-    DIV = {}
-
     line = line.strip().lower()
 
-    pat_1 = re.compile(r'([a-z]+)(\s+([0-7]))?(\s+([0-7]|\-?[0-9]+))?$', re.IGNORECASE)
-    m1 = pat_1.match(line)
+    xs = line.split()
 
-    pat_ldst = re.compile(r'([a-z]+)\s+([0-7])\s+(\-?[0-9]+\([0-7]\))$')
-    m2 = pat_ldst.match(line)
+    operation = xs[0]
 
-    if m1:
-        op = m1.group(1)
-        rs = m1.group(4)
-        rd = m1.group(2)
+    operand = xs[1:]
 
-        op1 = 0
-
-        if rd is None:
-            dest = 0
-        else:
-            dest = int(rd)
-
-        if rs is None:
-            src = 0
-        else:
-            src = int(rs)
-
-        if op == 'out':
-            src, dest = dest, src
-
-        if op in OP2:
-            op3 = 0b10
-            op2 = OP2[op]
-            if op in BRANCH:
-                dest = BRANCH[op]
-                d = sign_ext(dest, 8)
-            else:
-                d = sign_ext(src, 8)
-            return (op3 << 14) + (op2 << 11) + (dest << 8) + d
-        elif op in ARIN:
-            op3 = 0b11
-            op1 = ARIN[op]
-            if op in {'sll', 'slr', 'srl', 'sra'}:
-                d = src
-                return (op3 << 14) + (0 << 11) + (dest << 8) + (op1 << 4) + d
-        else:
-            raise ValueError('Invalid operation `{}`'.format(op))
-
-        return (op3 << 14) + (src << 11) + (dest << 8) + (op1 << 4)
-    elif m2:
-        op = m2.group(1)
-        ra = m2.group(2)
-        rbd = m2.group(3)
-
-        if op == 'ld':
-            op1 = 0b00
-        elif op == 'st':
-            op1 = 0b01
-
-        rai = int(ra)
-
-        rbi, d = parse_addr(rbd)
-        if d < 0:
-            d = 2 ** 8 + d
-
-        return (op1 << 14) + (rai << 11) + (rbi << 8) + d
+    if operation in ARIN:
+        op1 = 0b11
+    elif operation in OP2:
+        op1 = 0b10
+        d = parse_addr(operand[1])
+    elif operation == 'ld':
+        op1 = 0b00
+        d = parse_addr(operand[1])
+    elif operation == 'st':
+        op1 = 0b01
+        Ra = int(operand[0])
+        Rb, d = parse_addr(operand[1])
+        return (op1 << 14) + (Ra << 11) + (Rb << 8) + d
     else:
-        raise ValueError('Cannot parse `{}`'.format(line))
+        raise ValueError('Cannot parse `{}`',format(line))
 
 def main():
     cnt = 0
@@ -128,7 +84,7 @@ CONTENT BEGIN''')
 
     for line in sys.stdin:
         line = line.strip()
-        line = re.sub(r';(.+)$', '', line)
+        line = re.sub(r';.+$', '', line)
         if not line:
             continue
         print('  {:x}     :   {:016b}; -- {}'.format(cnt, parse_line(line), line))
@@ -138,3 +94,4 @@ CONTENT BEGIN''')
 
 if __name__ == '__main__':
     main()
+

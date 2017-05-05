4a5a6f62b2c7a9da9220b65e90d69329e2c0d768
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-05-05 17:19:02 +0900

    Use intermediate representation

---
+++ intermediate.py
@@ -0,0 +1,22 @@
+class Operation:
+    def __init__(self, op, ra, rb, d):
+        self.op = op
+        self.ra = ra
+        self.rb = rb
+        self.d = d
+
+    def __int__(self):
+        return (self.op << 14) + (self.ra << 11) + (self.rb << 8) + self.d
+
+    def __str__(self):
+        return 'Operation(op={0}, ra={1}, rb={2}, d={3})'.format(
+            self.op, self.ra, self.rb, self.d
+        )
+
+class Label:
+    def __init__(self, name, lineno):
+        self.name = name
+        self.lineno = lineno
+
+    def __str__(self):
+        return 'Label(name={0}, lineno={1})'.format(self.name, self.lineno)
